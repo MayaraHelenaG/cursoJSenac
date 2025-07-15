e39c79809efa2dbaebfb2141a13f5d03218840b5
Mayara Gonçalves <mayarahelenag@gmail.com>
2025-07-15 15:27:13 -0300

    :tada: commit inicial

---
+++ Aula 2/verifica.js
@@ -0,0 +1,13 @@
+
+
+let num = Number(prompt("Digite seu número aqui"));
+
+if (num % 2 === 0) {
+    alert(`Seu número ${num} é par`);
+} else {
+    alert(`Seu número ${num} é ímpar`);
+}
+
+
+
+document.getElementById('btnCalc').addEventListener('click', calculadora);
