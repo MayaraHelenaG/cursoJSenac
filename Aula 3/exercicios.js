7cdc674030c1791d40f37d128441fac38a8ea7a6
Mayara Gonçalves <mayarahelenag@gmail.com>
2025-07-25 18:06:13 -0300

    Adicionando exercícios

---
+++ Aula 3/exercicios.js
@@ -0,0 +1,31 @@
+//Lista de Exercícios – Laços de Repetição
+
+function calculadora() {
+    alert("calculadora");
+
+
+                        //Exercícios com for
+
+//1. Imprima os números de 1 a 30 que são múltiplos de 3.
+
+
+//2. Conte de 0 a 50 e diga se cada número é par ou ímpar.
+
+
+//3. Mostre a tabuada do número 6 (de 1 a 10).
+
+
+//4. Imprima os números de 10 a 0, em contagem regressiva.
+
+
+//5. Conte de 5 em 5 até 50 (5, 10, 15...).
+
+
+// 6. Para os números de 1 a 20, imprima: "Número X é menor que 10", "igual a 10" ou "maior que 10".
+
+
+
+
+}
+
+document.getElementById('btnCalc').addEventListener('click', calculadora);
